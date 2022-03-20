054453564f8a03e1468916e2c7c798804fb72a5d
FeNatalia <nataliafedorova1989@gmail.com>
2022-03-20 16:04:23 +0100

    saves in localstorage w-i-p

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from '../features/todoSlice';
+import { loadState, saveState } from './localStorage';
+import throttle from 'lodash.throttle';
 
-export default configureStore({
+const persistedState = loadState();
+
+const store = configureStore({
   reducer: {
     todos: todoReducer
   },
+  persistedState
 });
+
+store.subscribe(throttle(() => {
+  saveState({
+    todos: store.getState().todos
+  });
+}, 1000));
+
+export default store;
